277326b98d06e5d0ee29a8f8740c16f67f99fc23
Maryam Majedi <majedi@ucsb.edu>
2025-04-09 16:44:37 -0700

    week2

---
+++ week02/ASCII_simple.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c = 'A';
+    cout << "Character: " << c << endl;
+    cout << "ASCII code: " << int(c) << endl;
+    return 0;
+}
